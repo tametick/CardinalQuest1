445aab071aa6e3c495f5a4c6bcd777d74e5ae25f
Corey Martin <nefariousd@gmail.com>
2011-03-16 12:24:44 -0400

    Added some sounds to the Demo1 project for testing purposes.

--- src/Demo1/Resources.hx
+++ src/Demo1/Resources.hx
@@ -3,6 +3,8 @@ import flash.display.Bitmap;
 import flash.media.Sound;
 
 class FontSystem extends Font { public function new() { super(); } }
+class SoundSelect extends Sound { public function new() { super(); } }
+class SoundScroll extends Sound { public function new() { super(); } }
 
 class Resources
 {
