8abffa3dec8885cf017e77f4f2b1c6df24b3ad8b
Corey Martin <nefariousd@gmail.com>
2011-03-25 16:57:57 -0400

    Added placeholder splash screen state.

---
+++ src/cq/SplashState.hx
@@ -0,0 +1,48 @@
+package cq;
+
+import cq.CqResources;
+import haxel.HxlGraphics;
+import haxel.HxlState;
+import haxel.HxlText;
+import haxel.HxlTimer;
+
+class SplashState extends HxlState
+{
+
+	var fadeTimer:HxlTimer;
+	var fadeTime:Float;
+	var waitTime:Float;
+	var stateNum:Int;
+	var splashText:HxlText;
+
+	public override function create():Void {
+		super.create();
+
+		fadeTimer = new HxlTimer();
+		fadeTime = 0.5;
+		waitTime = 2.0;
+		stateNum = 0;
+
+		splashText = new HxlText(0, 220, 640, "Splash Screen");
+		splashText.setFormat(null, 40, 0xffffff, "center");
+		add(splashText);
+
+		HxlGraphics.fade.start(false, 0xff000000, fadeTime);
+	}
+
+	public override function update():Void {
+		super.update();	
+		
+		if ( stateNum == 0 && fadeTimer.delta() >= fadeTime ) {
+			fadeTimer.reset();
+			stateNum = 1;
+		} else if ( stateNum == 1 && fadeTimer.delta() >= waitTime ) {
+			stateNum = 2;
+			HxlGraphics.fade.start(true, 0xff000000, fadeTime, function() {
+				var newState = new GameState();
+				HxlGraphics.state = newState;
+			}, true);
+		}
+	}
+
+}
