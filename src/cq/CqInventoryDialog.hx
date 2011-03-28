324da63bd5096a6c1dd3b0e2fac9a4dae4c668d6
Corey Martin <nefariousd@gmail.com>
2011-03-28 13:18:36 -0400

    Implemented some fixes for HlxDialog backgrounds. Added some support code for active and disabled states on HxlButtons.

--- src/cq/CqInventoryDialog.hx
+++ src/cq/CqInventoryDialog.hx
@@ -1,7 +1,20 @@
 package cq;
 
+import haxel.HxlDialog;
 import haxel.HxlSlidingDialog;
 
 class CqInventoryDialog extends HxlSlidingDialog {
+
+	var dlgCharacter:HxlDialog;
+
+	public function new(?X:Float=0, ?Y:Float=0, ?Width:Float=100, ?Height:Float=100, ?Direction:Int=0)
+	{
+		// Size: 472 x 480
+		super(X, Y, Width, Height, Direction);
+
+		dlgCharacter = new HxlDialog(10, 10, 216, 220);
+		dlgCharacter.setBackgroundColor(0xff999999);
+		add(dlgCharacter);
+	}
 
 }
