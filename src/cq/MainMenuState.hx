a6fdfe101f617efdcca88fcdc8e4eb0873860ad0
Corey Martin <nefariousd@gmail.com>
2011-03-30 15:01:43 -0400

    Main Menu no longer fades in if displayed while game state is currently active. Main Menu will also have a working Resume Game option when this happens.

--- src/cq/MainMenuState.hx
+++ src/cq/MainMenuState.hx
@@ -16,6 +16,7 @@ class MainMenuState extends HxlState
 	var titleText:HxlText;
 
 	var menu:HxlMenu;
+	var btnResumeGame:HxlMenuItem;
 	var btnNewGame:HxlMenuItem;
 	var btnCredits:HxlMenuItem;
 
@@ -29,24 +30,39 @@ class MainMenuState extends HxlState
 		titleText.setFormat(null, 40, 0xff6666, "center");
 		add(titleText);
 
-		menu = new HxlMenu(220, 220, 200, 200);
+		menu = new HxlMenu(200, 220, 240, 200);
 		add(menu);
 
 		var self = this;
 
-		var btnNewGame:HxlMenuItem = new HxlMenuItem(0, 0, 200, "New Game");
+		var buttonY:Int = 0;
+
+		if ( stackId != 0 ) {
+			var btnResumeGame:HxlMenuItem = new HxlMenuItem(0, buttonY, 240, "Resume Game");
+			btnResumeGame.setNormalFormat(null, 40, 0xffffff, "center");
+			btnResumeGame.setHoverFormat(null, 40, 0xffff00, "center");
+			menu.addItem(btnResumeGame);
+			btnResumeGame.setCallback(function() { self.changeState(null); });
+			buttonY += 40;
+		}
+
+		var btnNewGame:HxlMenuItem = new HxlMenuItem(0, buttonY, 240, "New Game");
 		btnNewGame.setNormalFormat(null, 40, 0xffffff, "center");
 		btnNewGame.setHoverFormat(null, 40, 0xffff00, "center");
 		menu.addItem(btnNewGame);
 		btnNewGame.setCallback(function() { self.changeState(GameState); });
 
-		var btnCredits:HxlMenuItem = new HxlMenuItem(0, 40, 200, "Credits");
+		buttonY += 40;
+
+		var btnCredits:HxlMenuItem = new HxlMenuItem(0, buttonY, 240, "Credits");
 		btnCredits.setNormalFormat(null, 40, 0xffffff, "center");
 		btnCredits.setHoverFormat(null, 40, 0xffff00, "center");
 		menu.addItem(btnCredits);
 		btnCredits.setCallback(function() { self.changeState(CreditsState); });
 
-		HxlGraphics.fade.start(false, 0xff000000, fadeTime);
+		if ( stackId == 0 ) {
+			HxlGraphics.fade.start(false, 0xff000000, fadeTime);
+		}
 	}
 
 	public override function update():Void {
@@ -55,18 +71,21 @@ class MainMenuState extends HxlState
 
 	function changeState(TargetState:Class<HxlState>) {
 		var self = this;
+		if ( TargetState == null ) {
+			HxlGraphics.popState();
+			return;
+		}
 		HxlGraphics.fade.start(true, 0xff000000, fadeTime, function() {
 			var newState = Type.createInstance(TargetState, []);
-			//HxlGraphics.state = newState;
-			// Test code for state stacking
-			
-			if ( self.stackId == 0 ) {//|| TargetState == CreditsState ) {
+			if ( self.stackId == 0 ) {
 				HxlGraphics.state = newState;
 			} else {
-				if ( TargetState == CreditsState ) HxlGraphics.pushState(new CreditsState());
-				else HxlGraphics.popState();
-			}
-			
+				if ( TargetState == CreditsState ) {
+					HxlGraphics.pushState(new CreditsState());
+				} else {
+					HxlGraphics.state = newState;
+				}
+			}	
 		}, true);
 	}
 
