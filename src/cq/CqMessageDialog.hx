3b3b9dd9e98c38e1158e07e0499aff29ad0e1816
Corey Martin <nefariousd@gmail.com>
2011-04-05 12:47:19 -0400

    Added some basic styling to the log view state dialog.

--- src/cq/CqMessageDialog.hx
+++ src/cq/CqMessageDialog.hx
@@ -1,7 +1,13 @@
 package cq;
 
 import cq.CqResources;
+
+import flash.display.BitmapData;
+
+import haxel.HxlGradient;
+import haxel.HxlGraphics;
 import haxel.HxlSlidingDialog;
+import haxel.HxlSprite;
 import haxel.HxlTextContainer;
 
 class CqMessageDialog extends HxlSlidingDialog {
@@ -13,10 +19,13 @@ class CqMessageDialog extends HxlSlidingDialog {
 		// Size: 472 x 480
 		super(X, Y, Width, Height, Direction);
 
-		textBox = new HxlTextContainer( 0, 0, 472, 480 );
+		textBox = new HxlTextContainer( 10, 10, 452, 460 );
+		//textBox.setBackgroundSprite(HxlGradient.Rect(452, 40, [0xffffff, 0xffffff, 0xffffff], [0, 128, 255], [0.5, 0.0, 0.0], Math.PI/2, 20));
+		textBox.setBackgroundSprite(HxlGradient.Rect(452, 460, [0xd5d5d5, 0xcfcfcf, 0xbfbfbf, 0x505050, 0x333333], [0, 1, 2, 10, 255], null, Math.PI/2, 20));
+
 		textBox.reverseOrder = false;
 		textBox.maxLines = 100;
-		textBox.setFormat(null, 12, 0xffffff, "left", 0xff000000);
+		textBox.setFormat(null, 16, 0xffffff, "left", 0x010101);
 		add(textBox);
 		textBox.addText("Test 1..");
 		textBox.addText("Test 2..");
