8d7883308942654e6cf959e4e2a5324c9b3a5df6
Corey Martin <nefariousd@gmail.com>
2011-04-13 12:56:38 -0400

    Now displaying current and max health in character view state.

--- src/cq/CqCharacterDialog.hx
+++ src/cq/CqCharacterDialog.hx
@@ -15,12 +15,14 @@ class CqCharacterDialog extends HxlSlidingDialog {
 	var txtSpeedLabel:HxlText;
 	var txtSpiritLabel:HxlText;
 	var txtVitalityLabel:HxlText;
+	var txtHealthLabel:HxlText;
 
 	var valAttack:HxlText;
 	var valDefense:HxlText;
 	var valSpeed:HxlText;
 	var valSpirit:HxlText;
 	var valVitality:HxlText;
+	var valHealth:HxlText;
 
 	public function new(?X:Float=0, ?Y:Float=0, ?Width:Float=100, ?Height:Float=100, ?Direction:Int=0)
 	{
@@ -31,43 +33,51 @@ class CqCharacterDialog extends HxlSlidingDialog {
 		txtCharName.setFormat(null, 30, 0xffffff, "left", 0x010101);
 		add(txtCharName);
 
-		txtAttackLabel = new HxlText(20, 100, 430, "Attack:");
+		txtHealthLabel = new HxlText(20, 100, 430, "Health:");
+		txtHealthLabel.setFormat(null, 25, 0xffffff, "left", 0x010101);
+		add(txtHealthLabel);
+
+		valHealth = new HxlText(150, 100, 200, "0");
+		valHealth.setFormat(null, 25, 0xffffff, "left", 0x010101);
+		add(valHealth);
+
+		txtAttackLabel = new HxlText(20, 130, 430, "Attack:");
 		txtAttackLabel.setFormat(null, 25, 0xffffff, "left", 0x010101);
 		add(txtAttackLabel);
 
-		valAttack = new HxlText(150, 100, 200, "0");
+		valAttack = new HxlText(150, 130, 200, "0");
 		valAttack.setFormat(null, 25, 0xffffff, "left", 0x010101);
 		add(valAttack);
 
-		txtDefenseLabel = new HxlText(20, 130, 430, "Defense:");
+		txtDefenseLabel = new HxlText(20, 160, 430, "Defense:");
 		txtDefenseLabel.setFormat(null, 25, 0xffffff, "left", 0x010101);
 		add(txtDefenseLabel);
 
-		valDefense = new HxlText(150, 130, 200, "0");
+		valDefense = new HxlText(150, 160, 200, "0");
 		valDefense.setFormat(null, 25, 0xffffff, "left", 0x010101);
 		add(valDefense);
 
-		txtSpeedLabel = new HxlText(20, 160, 430, "Speed:");
+		txtSpeedLabel = new HxlText(20, 190, 430, "Speed:");
 		txtSpeedLabel.setFormat(null, 25, 0xffffff, "left", 0x010101);
 		add(txtSpeedLabel);
 
-		valSpeed = new HxlText(150, 160, 200, "0");
+		valSpeed = new HxlText(150, 190, 200, "0");
 		valSpeed.setFormat(null, 25, 0xffffff, "left", 0x010101);
 		add(valSpeed);
 
-		txtSpiritLabel = new HxlText(20, 190, 430, "Spirit:");
+		txtSpiritLabel = new HxlText(20, 220, 430, "Spirit:");
 		txtSpiritLabel.setFormat(null, 25, 0xffffff, "left", 0x010101);
 		add(txtSpiritLabel);
 
-		valSpirit = new HxlText(150, 190, 200, "0");
+		valSpirit = new HxlText(150, 220, 200, "0");
 		valSpirit.setFormat(null, 25, 0xffffff, "left", 0x010101);
 		add(valSpirit);
 
-		txtVitalityLabel = new HxlText(20, 220, 430, "Vitality:");
+		txtVitalityLabel = new HxlText(20, 250, 430, "Vitality:");
 		txtVitalityLabel.setFormat(null, 25, 0xffffff, "left", 0x010101);
 		add(txtVitalityLabel);
 
-		valVitality = new HxlText(150, 220, 200, "0");
+		valVitality = new HxlText(150, 250, 200, "0");
 		valVitality.setFormat(null, 25, 0xffffff, "left", 0x010101);
 		add(valVitality);
 
@@ -80,6 +90,7 @@ class CqCharacterDialog extends HxlSlidingDialog {
 
 	public function updateText():Void {
 		var _player:CqPlayer = cast(Registery.player, CqPlayer);
+		valHealth.text = ""+(_player.hp + _player.buffs.get("life"))+" / "+_player.maxHp;
 		valAttack.text = ""+(_player.attack + _player.buffs.get("attack"));
 		valDefense.text = ""+(_player.defense + _player.buffs.get("defense"));
 		valSpeed.text = ""+(_player.speed + _player.buffs.get("speed"));
