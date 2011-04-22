a31e6d91a94b88c6bc8954ee33c75eaa7726389e
Ido Yehieli <ido.yehieli@gmail.com>
2011-04-22 14:59:50 +0200

    update log out url

--- bin/password_protect.php
+++ bin/password_protect.php
@@ -56,7 +56,7 @@ $LOGIN_INFORMATION = array(
 define('USE_USERNAME', false);
 
 // User will be redirected to this page after logout
-define('LOGOUT_URL', 'http://www.example.com/');
+define('LOGOUT_URL', 'http://www.tametick.com/');
 
 // time out after NN minutes of inactivity. Set to 0 to not timeout
 define('TIMEOUT_MINUTES', 0);
