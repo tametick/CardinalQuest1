98882931011e27f4cd38685142d015ae0dd17a3e
Chris Beamond <Chris Beamond@localhost>
2011-05-19 09:13:40 +1000

    Added playtomic library and instructions for as3 to hx conversion.

---
+++ src/playtomic/Encode.hx
@@ -0,0 +1,9 @@
+package playtomic;
+
+extern class Encode {
+	function new() : Void;
+	static function Base64(p1 : flash.utils.ByteArray) : String;
+	static function Base64Decode(p1 : String) : flash.utils.ByteArray;
+	static function MD5(p1 : String) : String;
+	static function PNG(p1 : flash.display.BitmapData) : flash.utils.ByteArray;
+}
---
+++ src/playtomic/PlayerScore.hx
@@ -0,0 +1,12 @@
+package playtomic;
+
+@:final extern class PlayerScore {
+	var CustomData : Dynamic;
+	var FBUserId : String;
+	var Name : String;
+	var Points : Float;
+	var RDate : String;
+	var SDate : Date;
+	var Website : String;
+	function new() : Void;
+}
