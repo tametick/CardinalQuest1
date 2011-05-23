f065ded525f1555a015e97397f3dc9dcdf8ac61b
Ido Yehieli <ido.yehieli@gmail.com>
2011-05-23 19:43:08 +0200

    same spells can appear several times, denser levels

--- src/cq/CqConfiguration.hx
+++ src/cq/CqConfiguration.hx
@@ -3,9 +3,9 @@ package cq;
 import data.Configuration;
 
 class CqConfiguration extends Configuration {
-	public static var chestsPerLevel = 12;
-	public static var spellsPerLevel = 1;
-	public static var mobsPerLevel = 24;
+	public static var chestsPerLevel = 16;
+	public static var spellsPerLevel = 2;
+	public static var mobsPerLevel = 30;
 	public static var lastLevel = 7;
 	
 	public static function getLevelWidth(?level:Int=0) { 
