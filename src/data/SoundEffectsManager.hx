93c1664b211c052d845704f1a78035cc63dc517c
Ido Yehieli <ido.yehieli@gmail.com>
2011-07-05 18:24:40 +0200

    using footstep sounds

--- src/data/SoundEffectsManager.hx
+++ src/data/SoundEffectsManager.hx
@@ -5,7 +5,7 @@ import flash.media.Sound;
 import haxel.HxlSound;
 
 class SoundEffectsManager {
-	public static function play(effect:Class<Sound>) {
+	public static function play(effect:Class<Dynamic>) {
 		var snd = new HxlSound();
 		snd.loadEmbedded(effect);
 		snd.play();
