c937b9fb72e05609582f9ec7491bf23cfafb2f2b
Ido Yehieli <ido.yehieli@gmail.com>
2011-07-19 12:43:17 +0200

    CqEquipmentCell moved to its own file

--- src/cq/ui/CqPopup.hx
+++ src/cq/ui/CqPopup.hx
@@ -8,24 +8,16 @@ import haxel.HxlSprite;
 import haxel.HxlState;
 import haxel.HxlText;
 
-/**
- * ...
- * @author joris
- */
-
-class CqPopup extends HxlText
-{
+class CqPopup extends HxlText{
 	var parent:Dynamic;
-	public function new(Width:Int,Text:String,Parent:Dynamic) 
-	{
+	public function new(Width:Int,Text:String,Parent:Dynamic) {
 		parent = Parent;
 		super(0, 0, Width, Text);
 		setFormat("FontAnonymousPro", 15, 0xC2AC30, "left", 1);
 	}
-	public override function onRemove(state:HxlState):Void
-	{
+	
+	public override function onRemove(state:HxlState) {
 		parent.remove(this);
 	}
 	
-
 }
---
+++ src/cq/ui/inventory/CqEquipmentCell.hx
@@ -0,0 +1,38 @@
+package cq.ui.inventory;
+
+package cq.ui.inventory;
+import haxel.HxlDialog;
+import haxel.HxlSprite;
+import haxel.HxlText;
+import haxel.HxlGraphics;
+
+import cq.CqResources;
+
+// tmp
+import cq.ui.inventory.CqInventoryDialog;
+
+class CqEquipmentCell extends CqInventoryCell {
+
+	public static var highlightedCell:CqInventoryCell = null;
+	public var equipSlot:CqEquipSlot; // This should be read only
+	public var eqCellInit:Bool;
+	public var icon:HxlSprite;
+	public function new(EquipSlot:CqEquipSlot, ?X:Float=0, ?Y:Float=0, ?Width:Float=100, ?Height:Float=100, ?CellIndex:Int=0) {
+		super(X, Y, Width, Height, CellIndex);
+		equipSlot = EquipSlot;
+		eqCellInit = false;
+	}
+
+	override function setHighlighted(Toggle:Bool) {
+		if ( Toggle && cast(HxlGraphics.mouse.dragSprite, CqInventoryItem).item.equipSlot != equipSlot ) return; 
+		super.setHighlighted(Toggle);
+	}
+
+	public override function update() {
+		super.update();
+		if ( !eqCellInit ) {
+			eqCellInit = true;
+		}
+	}
+
+}
