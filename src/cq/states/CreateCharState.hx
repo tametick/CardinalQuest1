999b49804e396b26e40d5f4d152107bd1d8e8e0a
Ido Yehieli <ido.yehieli@gmail.com>
2011-07-28 13:05:20 +0200

    intro screen text

--- src/cq/states/CreateCharState.hx
+++ src/cq/states/CreateCharState.hx
@@ -163,8 +163,8 @@ class CreateCharState extends CqState {
 	}*/
 	
 	override function init() {
-		scroller = new CqTextScroller(IntroScreen, 1, "Intro screen");
-		var introText:String = "This is intro text\n a new line \n the end.";
+		scroller = new CqTextScroller(IntroScreen, 1);
+		var introText:String = " The evil minotaur Asterion has terrorized the peaceful land of Hallemot for countless years.\n\n In his underground den, he and his minions enjoy the spoils of their wicked deeds.\n\n Determined to end his reign of plunder and pillage, a single hero comes forth...";
 		scroller.addColumn(100, 400, introText, false, FontAnonymousPro.instance.fontName,30);
 		add(scroller);
 		scroller.startScroll();
