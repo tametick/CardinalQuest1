404745d07e1a0d093cd223246f00fdc20c6da3c8
Ido Yehieli <ido.yehieli@gmail.com>
2011-08-02 22:06:20 +0200

    various tweaks and small fixes

--- src/cq/states/CreateCharState.hx
+++ src/cq/states/CreateCharState.hx
@@ -61,6 +61,7 @@ class CreateCharState extends CqState {
 		});
 	}
 	function realInit() {
+		cursor.visible	= true;
 		if (scroller != null)
 			remove(scroller);
 		storyScreen = false;
@@ -163,10 +164,11 @@ class CreateCharState extends CqState {
 	}*/
 	
 	override function init() {
-		if(!shownIntro){
+		if (!shownIntro) {
+			cursor.visible	= false;
 			scroller = new CqTextScroller(IntroScreen, 1);
 			var introText:String = " The evil minotaur Asterion has terrorized the peaceful land of Hallemot for countless years.\n\n In his underground den, he and his minions enjoy the spoils of their wicked deeds.\n\n Determined to end his reign of plunder and pillage, a single hero comes forth...";
-			scroller.addColumn(80, 480, introText, false, FontAnonymousPro.instance.fontName,30);
+			scroller.addColumn(80, 480, introText, false, FontAnonymousPro.instance.fontName,30,0x000000,0x804040);
 			add(scroller);
 			scroller.startScroll();
 			//scroller.onComplete(removeScrollerAndFade);
