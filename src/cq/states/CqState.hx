e9c4e21c465e7db335eb3e59cb3e69b21a020451
jday <jday@localhost>
2011-10-06 09:25:39 -0700

    Monolithic commit of input and text changes * Mouse direction bug fixed * Mouse cursor rotation fixed * Keyboard timing fixed * Added smart sliding (although there's more to do) * Some text changes.  If you like them, we'll keep them

--- src/cq/states/CqState.hx
+++ src/cq/states/CqState.hx
@@ -6,6 +6,7 @@ import flash.geom.Rectangle;
 import flash.net.URLRequest;
 import haxel.HxlState;
 import haxel.HxlGraphics;
+import haxel.HxlPoint;
 import cq.CqResources;
 
 import flash.display.StageDisplayState;
@@ -16,22 +17,60 @@ class CqState extends HxlState {
 	public override function create() {
 		if(cursor==null){
 			cursor = new CursorSprite();
-			cursor.setFrame(SpriteCursor.instance.getSpriteIndex("diagonal"));
+			cursor.scrollFactor.y = cursor.scrollFactor.x = 0;
 		}
+		
+		setDiagonalCursor();
 		cursor.visible = true;
+		
 		super.create();
 	}
 	
-	function setDiagonalCursor() {
-		cursor.angle = 0;
-		var diagonal = SpriteCursor.instance.getSpriteIndex("diagonal");
-		if(cursor.getFrame()!=diagonal) {
-			cursor.setFrame(diagonal);
+	private function positionCursor() {
+		// override the default mouse positioning
+		if(cursor!=null) {
+			cursor.x = HxlGraphics.mouse.screenX - cursor.origin.x;
+			cursor.y = HxlGraphics.mouse.screenY - cursor.origin.y;
 		}
+	}
+	
+	function setDiagonalCursor(?facing:HxlPoint = null) {
+		// it would be nice to rename this setCursor, since it no longer only sets the diagonal cursor
+		
+		if (facing == null || (facing.x == 0 && facing.y == 0)) {
+			var diagonal = SpriteCursor.instance.getSpriteIndex("diagonal");
+			
+			// cursor rotation will set angle and the cursor origin
+			
+			cursor.angle = 0;
+			cursor.origin.x = 4;
+			cursor.origin.y = 2;
+			
+			if (cursor.getFrame() != diagonal) cursor.setFrame(diagonal);
+		} else {
+			var up:Int = SpriteCursor.instance.getSpriteIndex("up");
+			var newAngle:Float = cursor.angle;
+			
+			if (facing.x==0 && facing.y==1) newAngle = 180;
+			else if(facing.x==0 && facing.y==-1) newAngle = 0;
+			else if(facing.x==1 && facing.y==0) newAngle = 90
+			else if(facing.x==-1 && facing.y==0) newAngle = 270;
+			
+			if (cursor.getFrame() != up) cursor.setFrame(up);
+			if (cursor.angle != newAngle) cursor.angle = newAngle;
+			
+			cursor.origin.x = 16;
+			//cursor.origin.y = 3;
+			cursor.origin.y = 9;
+		}
+		
+		positionCursor();
 	}
 	
 	public override function update() {
 		super.update();
+		positionCursor();
+		
 		if (HxlGraphics.keys.justPressed("F2")) {
 			if(Lib.current.stage.displayState==StageDisplayState.NORMAL){
 				Lib.current.stage.displayState = StageDisplayState.FULL_SCREEN_INTERACTIVE;
